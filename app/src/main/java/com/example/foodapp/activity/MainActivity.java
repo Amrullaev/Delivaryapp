5f963119d35f346fc51dd70dda7652b74336b795
u.amrullayev <amrullayuevuchqun@gmail.com>
2023-08-31 13:26:21 +0500

    second

--- app/src/main/java/com/example/foodapp/activity/MainActivity.java
+++ app/src/main/java/com/example/foodapp/activity/MainActivity.java
@@ -1,16 +1,63 @@
 package com.example.foodapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.foodapp.R;
+import com.example.foodapp.adapter.CategoryAdapter;
+import com.example.foodapp.adapter.PopularAdapter;
+import com.example.foodapp.domain.CategoryDomain;
+import com.example.foodapp.domain.FoodDomain;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewCategoryList, recyclerViewPopularList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+        recylerViewPopular();
+    }
+
+    private void recyclerViewCategory() {
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category = new ArrayList<>();
+        category.add(new CategoryDomain("Pizza", "cat_1"));
+        category.add(new CategoryDomain("Burger", "cat_2"));
+        category.add(new CategoryDomain("HotDog", "cat_3"));
+        category.add(new CategoryDomain("Drink", "cat_4"));
+        category.add(new CategoryDomain("Donut", "cat_5"));
+
+        adapter = new CategoryAdapter(category);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+
+    private void recylerViewPopular() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewPopularList = findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodList = new ArrayList<>();
+
+        foodList.add(new FoodDomain("Peperoni Pizza", "pop_1", "slices pepperoni,mozzerella cheese,fresh oregano, ground black pepper,pizza sauce,", 9.76));
+        foodList.add(new FoodDomain("Cheese Burger", "pop_2", "beef,Gouda Cheese, Special Sauce, Lettuce, tomato", 8.79));
+        foodList.add(new FoodDomain("Vegetable pizza", "pop_3", "olive oil, Vegetable oil, pitted kalamata, cherry tomatoes, fresh oregano, basil", 8.5));
+
+        adapter2 = new PopularAdapter(foodList);
+        recyclerViewPopularList.setAdapter(adapter2);
+
     }
 }
